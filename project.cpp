7820eba54a2fee5e39623d9d0e6bcc488e8d60dd
Basil <basil.jose@student.tu-freiberg.de>
2022-03-01 20:58:02 +0100

    Implemented power method to obtain page rank vector.

--- project.cpp
+++ project.cpp
@@ -21,6 +21,18 @@ vector<float> matVec(vector<vector<float>> P, vector<float> r, const int n)
     return q;
 }
 
+float L1norm(vector<float> v, int n)
+{
+    float norm = 0;
+    for (int i = 0; i < n; i++)
+    {
+        norm += abs(v[i]);
+    }
+
+    return norm;
+
+}
+
 int main()
 {
     int n;
@@ -28,7 +40,6 @@ int main()
     cin >> n;
 
     // Create dense matrix
-    //int L[n][n] = {};
     vector<vector<float>> L(n);
     for (int i = 0 ; i < n ; i++) {
         L[i].resize(n, 0);
@@ -52,7 +63,11 @@ int main()
     L[5][4] = 1;
 
     // Create page-rank vector
-    float r[n] = {1};
+    vector<float> r(n);
+    for (int i = 0; i < n ; i++)
+    {
+        r[i] =1/float(n);
+    }
 
     int nLinks[n] = {0};
     float sum;
@@ -167,32 +182,103 @@ int main()
         }
     }
 
-    // test matrix-vector multiplication function
-    vector<vector<float>> X(3);
-    for (int i = 0 ; i < 3 ; i++) {
-        X[i].resize(3, 0);
+
+    // power iteration
+    vector<float> r_k_1(n);
+    for (int i = 0 ; i <n ; i++)
+    {
+        r_k_1[i] = r[i];
     }
-    X[1][1] = 8;
-    X[2][2] = 2.5;
-    X[2][1] = 21.23;
-    X[0][0] = 2.6;
 
-    vector<float> y(3);
-    y[0] = 1;
-    y[1] = 2;
-    y[2] = 3;
+    vector<float> r_k(n);
+    for (int i = 0 ; i <n ; i++)
+    {
+        r_k[i] = 0;
+    }
 
-    vector<float> Z(3);
-    int n_ = 3;
+    // Create q vector
+    vector<float> q_k(n);
+    for (int i = 0; i < n ; i++)
+    {
+        q_k[i] =0;
+    }
 
-    Z = matVec(X, y, n_);
+    vector<float> temp(n);
+    for (int i = 0; i < n ; i++)
+    {
+        temp[i] =0;
+    }
 
-    for (int row = 0; row < n; row++)
-    {   
-        for (int column = 0; column < n; column++)
+    int k = 1;
+    float q_k_norm = 0;
+
+    do 
+    {
+        q_k = matVec(P, r_k_1, n);
+        q_k_norm = L1norm(q_k, n);
+
+        for (int i = 0; i < n; i++) 
         {
-            cout << "P " << row << ' ' << column << ' ' << P[row][column] << endl;
+            r_k[i] = q_k[i]/q_k_norm;
+            temp[i] = r_k_1[i];
+            r_k_1[i] = r_k[i];
+            k++;
+                
         }
+
+    } while (abs(temp[0] - r_k_1[0]) < 0.0001);
+
+    cout << "iterations: " << k << endl;
+    float r_sum = 0.0;
+    for (int i = 0; i < k; i++)
+    {
+        r_sum += r_k[i];
     }
+    cout << "r_sum: " << r_sum << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "r_k " << r_k[i] << endl;
+    }
+
+    // // test matrix-vector multiplication function
+    // vector<vector<float>> X(3);
+    // for (int i = 0 ; i < 3 ; i++) {
+    //     X[i].resize(3, 0);
+    // }
+    // X[1][1] = 8;
+    // X[2][2] = 2.5;
+    // X[2][1] = 21.23;
+    // X[0][0] = 2.6;
+
+    // vector<float> y(3);
+    // y[0] = 1;
+    // y[1] = 2;
+    // y[2] = 3;
+
+    // vector<float> Z(3);
+    // int n_ = 3;
+
+    // Z = matVec(X, y, n_);
+
+    // // test L1 norm
+    // vector<float> v(n);
+    // for (int i = 0; i < n;  i++)
+    // {
+    //     v[i] = -3;
+    // }
+    // v[4] = 0;
+    // v[5] = 6;
+    // float norm;
+    // norm = L1norm(v, n);
+    // cout << norm << endl;
+
+    // for (int row = 0; row < n; row++)
+    // {   
+    //     for (int column = 0; column < n; column++)
+    //     {
+    //         cout << "P " << row << ' ' << column << ' ' << P[row][column] << endl;
+    //     }
+    // }
 }
 
